e10ade4d959194f536e946ffd322264fe758ec8f
Chris <chris@subooa.com.au>
2011-02-09 23:26:04 +1100

    Added in catch for feeds that are down, tries 5 times before giving up

--- rss_grab.php
+++ rss_grab.php
@@ -2,13 +2,39 @@
 	$find = array('\\', ' ');
 	$replace = array('', '+');
 	$url = str_replace($find, $replace, $_GET['url']);
+	
 
- 	$session = curl_init($url);
-	curl_setopt($session, CURLOPT_HEADER, false);
-	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($session, CURLOPT_FOLLOWLOCATION, true);
-	$xml = curl_exec($session);
-	header("Content-Type: text/xml");
-	echo $xml;
-	curl_close($session);
+	function get_rss($url, $try_count, $max_tries){
+	
+		$session = null;
+		try{
+		
+		 	$session = curl_init($url);
+			curl_setopt($session, CURLOPT_HEADER, false);
+			curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($session, CURLOPT_FOLLOWLOCATION, true);
+			$xml = curl_exec($session);
+			header("Content-Type: text/xml");
+			echo $xml;
+			curl_close($session);
+		
+		} catch(Exception $e) {
+		
+			curl_close($session);
+		
+			if($try_count < $max_tries){
+				get_rss($url, $tries+1, $max_tries);
+			} else {
+				echo "Connection issue";
+				return false;
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
+	get_rss($url, 0, 5);
 ?>
