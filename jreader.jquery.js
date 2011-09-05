cfa3226aa689e5eb349663e6793bce87c6d34eae
Chris Duell <chris@subooa.com.au>
2011-09-06 09:19:54 +1000

    Converted to full jQuery plugin, documentation added

---
+++ jreader.jquery.js
@@ -0,0 +1,67 @@
+/*****************************************************
+ * jReader, a jQuery plugin (coupled with PHP) to create a simple RSS feed reader
+ * by Chris Duell aka @duellsy of Subooa Studios
+ * 
+ * Version 0.3
+ * Full source available at http://github.com/duellsy/jreader
+ * Copyright © 2011 Subooa Studios http://subooa.com.au
+ *
+ * MIT License, https://github.com/duellsy/jreader/blob/master/LICENSE.md
+ ****************************************************/
+(function($){  
+	$.fn.jreader = function(options) { 
+
+		var defaults = {
+			loading: 'Loading',
+			titleElement: '#jReaderTitle',
+			fullArticle: "read full article",
+			articlesElement: '#jReaderArticles',
+			articleWrapClass: 'news-item',
+			grabberLocation: './'
+		};
+		var options = $.extend(defaults, options);
+
+
+		$(this).click(function(event){
+
+    		event.stopPropagation();
+
+    		$feed_title = $(this).html();
+
+    		$(options.titleElement).text(options.loading + ' ' + $feed_title + ' items');
+    		$(options.articlesElement).html('');		
+    		
+    		url = $(this).attr('href');
+
+    		$.get(options.grabberLocation + 'rss_grab.php?url=' + url, function(data){  
+    			$(options.titleElement).text($feed_title);
+    			
+    	        $(data).find('item,entry').each(function(){  
+
+    				var $article = $(this);  
+    				var title = $article.find("title").text();
+    				var description = $article.find("description").text();
+
+    				if(description == null || description == ''){
+    					description = $article.find("summary").text();
+    				}
+    				var link = $article.find("link").text();
+    				
+    				var new_item = '<div class="' + options.articleWrapClass + '">';
+    				new_item += '<h3 class="title">' + title + '</h3>';
+    				new_item += '<p class="description">' + description;  
+    				new_item += ' <a href=" ' + link + '" target="_blank">' + options.fullArticle + '</a></p>' ;  
+    				new_item += '</div>';
+    					  
+    				$(options.articlesElement).append($(new_item));
+    	  
+    	        }); 
+    	       
+    	    });
+    	    return false;
+    	    
+    	});			
+
+	}	
+
+})(jQuery);
